d8bb6a8868b73b8a2f69ed76e782a8a45e308395
sx6316 <1874737488@qq.com>
2021-07-03 02:48:39 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,68 @@
+# resolve-alias-from-ts-paths
+
+Used to replace `resolve.alias` of `webpack.config.jS` with `paths` of `tsconfig.json`
+
+## Install
+
+```shell
+npm install -D resolve-alias-from-ts-paths
+```
+
+## Example
+
+**`tsconfig.json`**
+
+```json
+{
+    "compilerOptions": {
+        "baseUrl": ".",
+        "paths": {
+            "/*":["./*"],
+            "@*": ["./src/*","./src/*/index"]
+        }
+}
+```
+
+The **`resolve.alias`** of `webpack` will become
+
+```
+{
+  '/': [ 'F:\\desktop\\react\\s' ],
+  '@': [ 'F:\\desktop\\react\\s\\src' ],
+  '@assets': [ 'F:\\desktop\\react\\s\\src\\assets' ],
+  '@utils': [ 'F:\\desktop\\react\\s\\src\\utils' ],
+  '@views': [ 'F:\\desktop\\react\\s\\src\\views' ]
+}
+```
+
+## Usage
+
+**`webpack.config.js`**
+
+```javascript
+const pathsToAlias = require('resolve-alias-from-ts-paths')
+
+module.exports = {
+    // ...
+    plugins: [new MP({ aliasLog: true })]
+}
+```
+
+or
+
+```js
+const pathsToAlias = require('resolve-alias-from-ts-paths')
+
+module.exports = {
+    // ...
+    resolve:{
+	  alias: pathsToAlias.getAlias()
+	}
+}
+```
+
+## Options
+ - `aliasLog` :  Boolean
+    - Show the replacement in your shell
+    - `defaule` : false
+
---
+++ index.js
@@ -0,0 +1,76 @@
+const fs = require("fs");
+const path = require("path");
+const sjc = require("strip-json-comments");
+const root = "../../";
+
+// TODO: 目录处理
+function pathResolve(...addrs) {
+	return path.resolve(__dirname, root, ...addrs);
+}
+// TODO: index文件处理
+function index(name, path) {
+	path = path.replace(/\/index(\.[^\/]+)?\/?$/, "");
+	if (path.match(/.*\/?.*\*[^\/]*\/?$/) && name.match(/^.*\/[^\/]*\*[^\/]*$/)) {
+		name = name === "/*" ? name : name.replace(/\/(?=[^\/]*\*[^\/]*$)/, "");
+
+		toAdd(name.replace(/\*\/?/, ""), path.replace(/\*/, ""));
+		return;
+	}
+	toAdd(name.replace(/\/?\*\/?/, ""), path.replace(/\*/, ""));
+	getAddir(path).forEach((em) => {
+		toAdd(name.replace(/\*/, em), path.replace(/\*/g, em));
+	});
+}
+// TODO: 获取 * 上级目录
+function getAddir(path) {
+	let name = path.match(/[^\/]+(?=\/?\*)/);
+	let addirs = fs.readdirSync(pathResolve(name[0]));
+	if (!addirs.length) return [];
+
+	let addiStr = "{" + addirs.join("}{") + "}";
+	addiStr = addiStr.replace(/\{[^\{\}]*\.[^\{\}]+\}/g, "");
+	addirs = addiStr.match(/[^\{\}]+/g);
+
+	return addirs;
+}
+// TODO: 规则添加
+function toAdd(name, path) {
+	path = pathResolve(_ts_baseUrl, path);
+	alias[name] = alias[name] || [];
+	alias[name][0] != path && alias[name].push(path);
+}
+
+function run() {
+	for (const name in _ts_paths) {
+		let paths = _ts_paths[name];
+		if (!name.match(/\*/)) {
+			alias[name] = paths.map((path) => pathResolve(_ts_baseUrl, path));
+			continue;
+		}
+		paths.forEach((path) => index(name, path));
+	}
+}
+
+const tsconfig = fs.readFileSync(pathResolve("tsconfig.json"), "utf8");
+const _ts_o = JSON.parse(sjc(tsconfig));
+const _ts_paths = _ts_o.compilerOptions.paths || {};
+const _ts_baseUrl = _ts_o.compilerOptions.baseUrl || "./";
+let alias = {};
+
+class resolveAliasFromTsPaths {
+	constructor({ aliasLog = false }) {
+		run();
+		if (aliasLog) {
+			console.log("The 'resolve.alias' will be replaced with the following:");
+			console.log(alias);
+		}
+	}
+	apply(compiler) {
+		compiler.options.resolve.alias = alias;
+	}
+	static getAlias() {
+		return alias;
+	}
+}
+
+module.exports = resolveAliasFromTsPaths;
